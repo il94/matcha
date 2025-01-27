9ec45a06e9d4254104e861c91848c0977e3a74ac
il94 <ilyes.landolsi@outlook.fr>
2025-01-27 13:55:03 +0100

    feat: create Profile page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import MobileLayout from "./views/mobile/MobileLayout"
-import Home from "./views/mobile/home/Home"
+import Home from "./views/mobile/home"
 import { ThemeProvider } from "./providers/ThemeProvider"
+import Profile from "./views/mobile/profile"
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<MobileLayout />}>
 						<Route index element={<Home />} />
+						<Route path="profile" element={<Profile />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
---
+++ front/src/views/mobile/profile/index.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button"
+
+type StatProps = {
+	value: number
+	label: string
+}
+
+const Stat = ({ value, label }: StatProps) => (
+	<div className="flex flex-col items-center">
+		<p className="text-3xl font-bold">{value}</p>
+		<p className="text-1xl">{label}</p>
+	</div>
+)
+
+export default function Profile() {
+	const user = {
+		firstName: "Loremosowddsd",
+		profilePicture: "/model.JPG",
+		elo: 48,
+		views: 128,
+		matchs: 204,
+		dates: 203,
+	}
+
+	return (
+		<div className="flex h-full flex-col items-center justify-between overflow-y-hidden px-3 py-8">
+			<h2 className="text-4xl">Hi, {user.firstName}</h2>
+
+			<div className="flex h-full w-full flex-col items-center justify-evenly">
+				<img
+					src={user.profilePicture}
+					className="size-32 rounded-full object-cover"
+				/>
+				<div className="flex w-full justify-evenly">
+					<Stat value={user.elo} label="Rating" />
+					<Stat value={user.views} label="Views" />
+				</div>
+				<div className="flex w-full justify-evenly">
+					<Stat value={user.matchs} label="Matchs" />
+					<Stat value={user.dates} label="Dates" />
+				</div>
+			</div>
+
+			<div className="w-full space-y-3">
+				<Button variant="dark" className="h-10 w-full rounded-xl">
+					Edit profile
+				</Button>
+				<Button variant="dark" className="h-10 w-full rounded-xl">
+					Preview
+				</Button>
+			</div>
+		</div>
+	)
+}
