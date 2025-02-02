db3645f939301cb2e03d1ebbbef2ba284f566ec7
il94 <ilyes.landolsi@outlook.fr>
2025-02-02 18:48:45 +0100

    feat: add Link to footerNavbar

--- front/src/views/mobile/FooterNavbar.tsx
+++ front/src/views/mobile/FooterNavbar.tsx
@@ -1,11 +1,25 @@
+import { Button } from "@/components/ui/button"
 import { HouseIcon, MessageCircleIcon, UserIcon } from "lucide-react"
+import { Link } from "react-router"
 
 export default function FooterNavbar() {
 	return (
 		<nav className="flex h-14 shrink-0 items-center justify-evenly bg-secondary">
-			<HouseIcon className="size-8 text-black" />
-			<MessageCircleIcon className="size-8 text-black" />
-			<UserIcon className="size-8 text-black" />
+			<Button asChild size="icon" variant="secondary">
+				<Link to="/">
+					<HouseIcon className="size-8 text-black" />
+				</Link>
+			</Button>
+			<Button asChild size="icon" variant="secondary">
+				<Link to="/chat">
+					<MessageCircleIcon className="size-8 text-black" />
+				</Link>
+			</Button>
+			<Button asChild size="icon" variant="secondary">
+				<Link to="/profile">
+					<UserIcon className="size-8 text-black" />
+				</Link>
+			</Button>
 		</nav>
 	)
 }
