9d6adea2885476b37f626cd2406628fd7a7e02fd
il94 <ilyes.landolsi@outlook.fr>
2025-05-19 15:06:09 +0200

    fix: get uniques userchats

--- back/src/db/queries/app/getUserChatsQuery.ts
+++ back/src/db/queries/app/getUserChatsQuery.ts
@@ -5,7 +5,7 @@ export const getUserChatsQuery = `
 		pictures.name AS avatar,
 		(SELECT JSON_BUILD_OBJECT('content', messages.content, 'createdAt', messages.created_at) FROM messages WHERE messages.chat_id = chats.id ORDER BY messages.created_at DESC LIMIT 1) AS last_message
 	FROM chats
-	LEFT JOIN users ON users.id = user_id_1 OR users.id = user_id_2 AND users.id != $1
+	LEFT JOIN users ON (users.id = user_id_1 AND users.id != $1) OR (users.id = user_id_2 AND users.id != $1)
 	LEFT JOIN pictures ON users.id = pictures.user_id AND pictures.is_principal = true
 	WHERE user_id_1 = $1 OR user_id_2 = $1;
 `
