f72996f9a34a286f79bbe5e4ea2337f22ec06301
il94 <ilyes.landolsi@outlook.fr>
2025-05-27 14:41:33 +0200

    fix: add correct parameter numbers on createusermutation

--- back/src/db/queries/admin/createUserMutation.ts
+++ back/src/db/queries/admin/createUserMutation.ts
@@ -18,6 +18,6 @@ export const createUserMutation = `
 		activated,
 		completed
 	)
-	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
 	RETURNING id;
 `
