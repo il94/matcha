d79a278f4c72d3fb7ee29aa021a97d8c337cc772
il94 <ilyes.landolsi@outlook.fr>
2025-06-03 15:15:24 +0200

    fix: hide animation on AnimateHalo on off

--- front/src/components/AnimateHalo.tsx
+++ front/src/components/AnimateHalo.tsx
@@ -42,7 +42,7 @@ export default function AnimateHalo({
 		<div className={cn("relative size-4", className, size && sizes[size - 1])}>
 			<motion.div
 				className={cn(
-					"absolute left-1/2 top-1/2 z-10 size-2/3 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full bg-gradient-to-tr from-green-500 to-green-200",
+					"absolute left-1/2 top-1/2 size-2/3 -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-full bg-gradient-to-tr from-green-500 to-green-200",
 					off && "from-yellow-500 to-yellow-200",
 				)}
 				animate={off ? false : { backgroundPositionX: "100%" }}
@@ -56,26 +56,28 @@ export default function AnimateHalo({
 				}}
 				style={{ backgroundSize: "200%" }}
 			/>
-			<motion.div
-				className={cn(
-					"opacity-0.5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-green-300 p-0",
-					size && borders[Math.floor(size / 2)],
-				)}
-				animate={off ? false : { padding: "65%", opacity: 0 }}
-				transition={{
-					padding: {
-						repeat: Infinity,
-						duration: 1,
-						delay: 0.25,
-					},
-					opacity: {
-						repeat: Infinity,
-						duration: 1,
-						ease: "easeInOut",
-						delay: 0.5,
-					},
-				}}
-			/>
+			{!off && (
+				<motion.div
+					className={cn(
+						"opacity-0.5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-green-300 p-0",
+						size && borders[Math.floor(size / 2)],
+					)}
+					animate={{ padding: "65%", opacity: 0 }}
+					transition={{
+						padding: {
+							repeat: Infinity,
+							duration: 1,
+							delay: 0.25,
+						},
+						opacity: {
+							repeat: Infinity,
+							duration: 1,
+							ease: "easeInOut",
+							delay: 0.5,
+						},
+					}}
+				/>
+			)}
 		</div>
 	)
 }
