181370fe5005542da386df39ef0461cc5847c992
il94 <ilyes.landolsi@outlook.fr>
2026-07-13 22:47:19 +0200

    fix: remove redundant condition from deleteInactiveUsersMutation query for clarity

--- back/src/db/queries/app/deleteInactiveUsersMutation.ts
+++ back/src/db/queries/app/deleteInactiveUsersMutation.ts
@@ -1,6 +1,5 @@
 export const deleteInactiveUsersMutation = `
   DELETE FROM users
-  WHERE (activated = FALSE AND created_at < NOW() - INTERVAL '1 day')
-     OR first_name = 'haha'
+  WHERE activated = FALSE AND created_at < NOW() - INTERVAL '1 day'
   RETURNING id, email, username;
 `
