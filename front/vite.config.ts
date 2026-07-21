6b1bf501c43e78405b371fb0c01f33dc86b4e425
il94 <ilyes.landolsi@outlook.fr>
2026-07-21 18:50:41 +0200

    chore: update Vite preview configuration to enable host access and specify allowed hosts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
 	plugins: [react()],
+	preview: {
+		host: true,
+		port: 4009,
+		allowedHosts: ["matcha.ilandols.com"],
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
