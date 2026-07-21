3fb1d5a395975ffd8513fb7f8f273651c4d0026d
il94 <ilyes.landolsi@outlook.fr>
2026-07-21 18:31:06 +0200

    fix: support Redis auth via REDIS_PASSWORD

--- back/src/redis/redis.plugin.ts
+++ back/src/redis/redis.plugin.ts
@@ -2,7 +2,10 @@ import fastifyRedis from "@fastify/redis"
 import { FastifyPluginAsync } from "fastify"
 
 const redisPlugin: FastifyPluginAsync = async (app, options) => {
-	app.register(fastifyRedis, { host: process.env.REDIS_HOST })
+	app.register(fastifyRedis, {
+		host: process.env.REDIS_HOST,
+		password: process.env.REDIS_PASSWORD,
+	})
 }
 
 export default redisPlugin
